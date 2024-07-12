d0c51042471b2552169d7bc70fc8ca060d8f850f
Lohit-kk <Lohit_K_Kodkani@Dell.com>
2024-07-12 16:02:24 +0530

    Reboot

--- Reboot.ps1
+++ Reboot.ps1
@@ -1,5 +1,5 @@
 # Get-RebootShutdownLogs.ps1
 $logPath = "C:\Temp\System.evtx"
-Get-WinEvent -Path $logPath -FilterHashtable @{LogName='System'; Id=1074} | 
+Get-WinEvent -Path $logPath | Where-Object { $_.Id -eq 1074 } | 
 Select-Object Id, TimeCreated, LevelDisplayName, Message | 
 Format-List
